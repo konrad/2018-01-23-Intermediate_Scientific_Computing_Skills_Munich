7b1ffd27a5521aa5f913ffe2cb4c0fb079207312
Konrad Förstner <konrad@foerstner.org>
2018-01-24 22:09:09 +0100

    Improve shell script (readonly and comment)

--- Shell/fasta_and_gff_analysis.sh
+++ Shell/fasta_and_gff_analysis.sh
@@ -1,11 +1,11 @@
-# This script download a fasta and a gff file and analyze them
+# This script downloads a fasta and a gff file and analyze them
 # 2018-01-23 by Konrad Förstner
 
 main(){
     echo "* Starting my analysis"
     readonly NCBI_FTP="ftp://ftp.ncbi.nih.gov/genomes/archive/old_refseq/Bacteria/Salmonella_enterica_serovar_Typhimurium_SL1344_uid86645"
-    INPUT_FOLDER="input"
-    OUTPUT_FOLDER="output"
+    readonly INPUT_FOLDER="input"
+    readonly OUTPUT_FOLDER="output"
     create_folders
     download_fasta
     count_entries
@@ -19,6 +19,7 @@ create_folders(){
 download_fasta(){
    echo "* Downloading fasta files"
    wget -c -P ${INPUT_FOLDER} ${NCBI_FTP}/NC_016810.ffn
+   # Alternative if wget ist noch installed
    # curl ${NCBI_FTP}/NC_016810.ffn > ${INPUT_FOLDER}/NC_016810.ffn
 }
 
